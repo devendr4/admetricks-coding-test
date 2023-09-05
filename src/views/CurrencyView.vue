39119319ff29f7bf508d90c7474b14223a23c5dd
devendr4 <angel.emo.altuve@gmail.com>
2023-09-05 00:43:12 -0400

    currency view

---
+++ src/views/CurrencyView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import CustomTitle from '@/components/CustomTitle.vue'
+</script>
+
+<template>
+  <main class="relative min-h-screen max-h-screen md:mt-80 md:ml-20% md:mr-20% ml-[5%] mr-5%">
+    <CustomTitle>Peso chileno</CustomTitle>
+  </main>
+</template>
